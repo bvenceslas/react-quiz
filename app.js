7b6f517c20dae69f5d7ee2a41ec4b3926047e1a9
bvenceslas <bvenceslas@gmail.com>
2020-12-18 05:42:32 +0200

    get response from a json file

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const http = require('http');
-
+const fs = require('fs');
 // creating the server using express
 const app = express();
 const server = http.createServer(app);
 
 // get endpoint
 app.get('/', (req, res) => {
-    res.send('<h1>API works fine</h1>');
+    res.send('<h1>API works fine ...</h1>'); // res.end() also can be used
 });
+
+app.get('/data', (req, res) => {
+    fs.readFile('./db.json', (err, data) => {
+        if(!err) res.json(data.toString())
+    });
+})
 
 
 
